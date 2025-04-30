4b2eebb82159ac30d02ea73331884c9364fca2b4
Dessalines <tyhou13@gmx.com>
2025-04-29 21:58:51 -0400

    Initial commit.

---
+++ app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    alias(libs.plugins.android.application)
+    id("com.google.wff.validatorplugin")
+}
+
+android {
+    namespace = "com.dessalines.lcd_black"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.dessalines.lcd_black"
+        minSdk = 34
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    if (project.hasProperty("RELEASE_STORE_FILE")) {
+        signingConfigs {
+            create("release") {
+                storeFile = file(project.property("RELEASE_STORE_FILE")!!)
+                storePassword = project.property("RELEASE_STORE_PASSWORD") as String?
+                keyAlias = project.property("RELEASE_KEY_ALIAS") as String?
+                keyPassword = project.property("RELEASE_KEY_PASSWORD") as String?
+
+                // Optional, specify signing versions used
+                enableV1Signing = true
+                enableV2Signing = true
+            }
+        }
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = true
+        }
+        release {
+            if (project.hasProperty("RELEASE_STORE_FILE")) {
+                signingConfig = signingConfigs.getByName("release")
+            }
+            isMinifyEnabled = true
+            // Ensure shrink resources is false, to avoid potential for them
+            // being removed.
+            isShrinkResources = false
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    includeBuild("../validator-plugin")
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+gradle.startParameter.excludedTaskNames.addAll(listOf(":validator-plugin:plugins:testClasses"))
+
+rootProject.name = "LCD-black"
+include(":app")
+ 
