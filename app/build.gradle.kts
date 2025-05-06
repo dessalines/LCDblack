7474ccae1afe02ce714ff83794f7ce56f28ba015
Dessalines <dessalines@users.noreply.github.com>
2025-05-05 22:35:57 -0400

    Adding a larger clock, text colors, and AOD modes. (#1)
    - Also getting rid of two side complications.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.dessalines.lcd_black"
         minSdk = 34
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     if (project.hasProperty("RELEASE_STORE_FILE")) {
