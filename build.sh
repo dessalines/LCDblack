b96e174644c7bbdf2975c8416d58de3b74c145d1
Dessalines <tyhou13@gmx.com>
2025-07-29 10:10:50 -0400

    Adding generate faces.

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./gradlew clean
+./gradlew assembleRelease
+./gradlew bundleRelease
+
+cp app/build/outputs/apk/release/app-release.apk ~/Sync/
+cp app/build/outputs/bundle/release/app-release.aab ~/Sync/
