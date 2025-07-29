959695adaae6b99ce8480c714641e41bd51e8595
Dessalines <dessalines@users.noreply.github.com>
2025-07-29 10:15:30 -0400

    Simplify generate face scripts. (#3)

--- generate.sh
+++ generate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Generate the face
-pushd templates
-./generate_face.sh
-popd
+export COLORS=$(cat templates/snippets/colors.xml)
+export TOP_ICON_COMPLICATION=$(cat templates/snippets/top_icon_complication.xml)
+export BOTTOM_ICON_COMPLICATION=$(cat templates/snippets/bottom_icon_complication.xml)
+
+cat templates/main.xml | envsubst >app/src/main/res/raw/watchface.xml
 
 # Format the face
 xmllint -o app/src/main/res/raw/watchface.xml --format app/src/main/res/raw/watchface.xml
--- templates/generate_face.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export COLORS=$(cat snippets/colors.xml)
-export TOP_ICON_COMPLICATION=$(cat snippets/top_icon_complication.xml)
-export BOTTOM_ICON_COMPLICATION=$(cat snippets/bottom_icon_complication.xml)
-
-cat main.xml | envsubst >../app/src/main/res/raw/watchface.xml
